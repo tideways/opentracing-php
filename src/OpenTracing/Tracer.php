1b8b8a0e29e5accaa6dbfaf7a43f65456ff05ab2
Benjamin Eberlei <kontakt@beberlei.de>
2017-03-27 11:31:12 +0200

    Added Tracer#flush() method based on discussion in opentracing/public gitter chat on March 25th-27th.

--- src/OpenTracing/Tracer.php
+++ src/OpenTracing/Tracer.php
@@ -9,4 +9,19 @@ interface Tracer
     public function inject(SpanContext $context, $format, $carrier);
 
     public function extract($format, $carrier) : SpanContext;
+
+    /**
+     * Allow tracer to transmit span data out of process.
+     *
+     * This method may not be needed depending on the tracer implementation,
+     * but every user should make sure to call it at the end of the PHP
+     * request.  In PHP-FPM based applictions it makes sense to wait after the
+     * call to {@see fastcgi_finish_request} to avoid blocking the user
+     * request.
+     *
+     * OpenTracing does not contain this method in other APIs, because they
+     * usually can move spans to other threads whenever a Span is finished at
+     * very low cost before sending them out of process.
+     */
+    public function flush();
 }
